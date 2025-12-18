872a79d56a311578d14f1635be5850e579ab1949
Reiduarig <giraudier82@gmail.com>
2025-12-18 13:04:27 +0100

    feature: resource para cartItem

---
+++ app/Http/Resources/CartItemResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CartItemResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'event' => new EventResource($this->whenLoaded('event')),
+            'quantity' => $this->quantity,
+            'price_snapshot' => $this->price_snapshot,
+            'subtotal' => $this->subtotal,
+            'created_at' => $this->created_at->toIso8601String(),
+            'updated_at' => $this->updated_at->toIso8601String(),
+        ];
+    }
+}
